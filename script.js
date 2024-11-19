762e0c7ec1dce873119a4807460915082586f0fc
victoriarahme <vic.rahme@gmail.com>
2024-11-19 18:02:40 -0500

    Added styling, made edits to js file

--- script.js
+++ script.js
@@ -3,9 +3,11 @@
         var hoverDiv = document.getElementById('hoverDiv');
         hoverDiv.addEventListener('mouseenter', function() {
             this.style.backgroundColor = 'lightblue';
+            console.log('Mouse has hovered the button ');
         });
         hoverDiv.addEventListener('mouseleave', function() {
             this.style.backgroundColor = 'transparent';
+            console.log('Mouse has stopped hovering the button');
         });
 //Keydown event
 
@@ -14,6 +16,9 @@
             if (event.key === 'Escape') {
                 console.log('Escape key was pressed.');
             }
+        });
+        document.getElementById('pressKey').addEventListener('keydown', function(event) {
+            console.log(`Key pressed in input: ${event.key}`);
         });
 
 //Form event
@@ -37,3 +42,26 @@
                 console.log('Input changed: ', this.value);
              });
             });
+//focus, blur event
+
+            function handleFocus(event) {
+                console.log(event);
+                console.log(event.target);
+                console.log(event.target.value);
+                event.target.style.background = 'lightblue';
+            }
+
+    function validateWeight() {
+        var weight = document.getElementById('weightInput');
+        var weightValue =parseInt(weight.value, 10);
+//I put 90 as minimum adult weight (if a 4'11 woman for example), other wise anything below considered a child
+        if (weightValue >= 90) {
+            weight.style.backgroundColor = 'lightgreen';
+            console.log('Eligible adult weight confirmed.');
+         }
+            else {
+                weight.style.backgroundColor = 'Salmon';
+                console.log('Check weight, may be under adult eligibility.');
+            }
+        }
+    
